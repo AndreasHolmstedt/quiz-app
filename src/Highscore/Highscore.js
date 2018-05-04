eee2d31eaeedc896c05e0aa48eb67501c8105b67
Asbert75 <erik_olsson_97@hotmail.com>
2018-05-04 13:31:55 +0200

    Continued work on Highscore page

--- src/Highscore/Highscore.js
+++ src/Highscore/Highscore.js
@@ -15,6 +15,7 @@ class Highscore extends Component {
         this.props.database.ref("/users/").once("value", snapshot => {
             let data = snapshot.val();
             let highscores = [];
+
             for(let score in data) {
                 if(data.hasOwnProperty(score)) {
                     highscores.push(data[score]);
@@ -43,15 +44,26 @@ class Highscore extends Component {
                 //spelarens highscore
                 //lista med högsta poäng för alla användare
                 }
+                <div className="userStats">
+                    <h3 className="personalHighscore">Your Highscore: 250</h3>
+                    <img className="avatar" src="http://avatarbox.net/avatars/img19/47_face_avatar_picture_12669.jpg" alt="avatar"  />
+                    <p className="player">You</p>
+                </div>
                 {
                     this.state.isLoading
                     ? <p>Loading Highscores...</p>
                     :   <ul>
+                            <li>
+                                <p className="rankHeader">Rank</p>
+                                <p className="scoreHeader">Score</p>
+                                <p className="playerHeader">Player</p>
+                            </li>
                             { this.state.highscores.map( (score, index) => (
                                     <li key={score.nick}>
-                                        <img src={score.img} alt="avatar" />
-                                        <p>{index+1} - {score.nick}</p>
-                                        <h3>{score.highscore}</h3>
+                                        <h3 className="rank">{index+1}</h3>
+                                        <h3 className="score">{score.highscore}</h3>
+                                        <img className="avatar" src={score.img} alt="avatar"  />
+                                        <p className="player">{score.nick}</p>
                                     </li> )
                                 )
                             }
